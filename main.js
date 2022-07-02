6e5bd511bc01f6fd4a33b7d2dc49ec4086a08ab0
FOLORUNSHO QUAZEEM <31304853+saintsquazeem@users.noreply.github.com>
2022-07-02 13:56:11 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,112 @@
+ let name1 = {firstname:"Thompson", Lastname: "Anthony", Age: 32, Sex: "Male", Position: "Head of Finance", enroledate
+ :"17/07/2014",};
+ let name2 = {firstname:"Jhonson", Lastname: "Mary", Age: 23, Sex: "female", Position: "software engeneer", enroledate
+ :"05/05/2019",};
+ let name3 = {firstname:"Chisom", Lastname: "Onyeka", Age: 28, Sex: "Male", Position: "Human Resource Manager", enroledate:"12/07/2016",};
+ let name4 = {firstname:"Jide", Lastname: "Olabisi", Age: 38, Sex: "Male", Position: " Assistant Head of Finance", enroledate:"17/07/2014",};
+ let name5 = {firstname:"Williams", Lastname: "Ajayi", Age: 30, Sex: "Female", Position: "Secetary", enroledate
+ :"12/05/2015",};
+ let name6 = {firstname:"Nancy", Lastname: "ChukwuEmeka", Age: 27, Sex: "Male", Position: "Head of Software department", enroledate:"17/07/2014",};
+ let name7 = {firstname:"Nicholas", Lastname: "Uchemba", Age: 26, Sex: "Male", Position: "software engeneer", enroledate
+ :"12/03/2015",};
+ let name8 = {firstname:"Ahmmed", Lastname: "Muhammed", Age: 24, Sex: "femaale", Position: "Graphic Designer", enroledate
+ :"19/04/2020",};
+ let name9 = {firstname:"Hamed", Lastname: "Adeyeri", Age: 26, Sex: "Male", Position: "Senior Finance", enroledate
+ :"27/08/2020",};
+ let name10 = {firstname:"Richard", Lastname: "Olayinka", Age: 41, Sex: "Male", Position: "CHEIF EXECUTIVE", enroledate
+ :"30/05/2014",};
+
+ let tablehead = {firstnom:"NO", secondnom:"FIRSTNAME", thirdnom:"LASTNAME", fourthnom:"AGE", fifthnom:"SEX", sixthnom:"POSITION",seventhnom:"DATE OF ENROLEMENT", };
+
+
+document.getElementById("nom").innerHTML=tablehead.firstnom;
+document.getElementById("FIRSTN").innerHTML=tablehead.secondnom;
+document.getElementById("Lastn").innerHTML=tablehead.thirdnom;
+document.getElementById("AGE_N").innerHTML=tablehead.fourthnom;
+document.getElementById("SEX_N").innerHTML=tablehead.fifthnom;
+document.getElementById("POSITION_N").innerHTML=tablehead.sixthnom;
+document.getElementById("_DOE_N").innerHTML=tablehead.seventhnom;
+
+ 
+ 
+ //FIRST ROW
+ document.getElementById("firstname1").innerHTML=name1.firstname;
+ document.getElementById("lastname1").innerHTML=name1.Lastname;
+ document.getElementById("age1").innerHTML=name1.Age;
+ document.getElementById("sex1").innerHTML=name1.Sex;
+ document.getElementById("position1").innerHTML=name1.Position;
+ document.getElementById("deo1").innerHTML=name1.enroledate;
+ 
+ //SECOND ROW
+ document.getElementById("firstname2").innerHTML=name2.firstname;
+ document.getElementById("lastname2").innerHTML=name2.Lastname;
+ document.getElementById("age2").innerHTML=name2.Age;
+ document.getElementById("sex2").innerHTML=name2.Sex;
+ document.getElementById("position2").innerHTML=name2.Position;
+ document.getElementById("deo2").innerHTML=name2.enroledate;
+ 
+ //THIRD ROW
+ document.getElementById("firstname3").innerHTML=name3.firstname;
+ document.getElementById("lastname3").innerHTML=name3.Lastname;
+ document.getElementById("age3").innerHTML=name3.Age;
+ document.getElementById("sex3").innerHTML=name4.Sex;
+ document.getElementById("position3").innerHTML=name4.Position;
+ document.getElementById("deo3").innerHTML=name4.enroledate;
+
+ //FOURTH ROW
+ document.getElementById("firstname4").innerHTML=name4.firstname;
+ document.getElementById("lastname4").innerHTML=name4.Lastname;
+ document.getElementById("age4").innerHTML=name4.Age;
+ document.getElementById("sex4").innerHTML=name4.Sex;
+ document.getElementById("position4").innerHTML=name4.Position;
+ document.getElementById("deo4").innerHTML=name4.enroledate;
+
+ //FIFTH ROW
+ document.getElementById("firstname5").innerHTML=name5.firstname;
+ document.getElementById("lastname5").innerHTML=name5.Lastname;
+ document.getElementById("age5").innerHTML=name5.Age;
+ document.getElementById("sex5").innerHTML=name5.Sex;
+ document.getElementById("position5").innerHTML=name5.Position;
+ document.getElementById("deo5").innerHTML=name5.enroledate;
+
+ //SIX ROW
+ document.getElementById("firstname6").innerHTML=name6.firstname;
+ document.getElementById("lastname6").innerHTML=name6.Lastname;
+ document.getElementById("age6").innerHTML=name6.Age;
+ document.getElementById("sex6").innerHTML=name6.Sex;
+ document.getElementById("position6").innerHTML=name6.Position;
+ document.getElementById("deo6").innerHTML=name6.enroledate;
+ 
+ //SEVEN ROW
+ document.getElementById("firstname7").innerHTML=name7.firstname;
+ document.getElementById("lastname7").innerHTML=name7.Lastname;
+ document.getElementById("age7").innerHTML=name7.Age;
+ document.getElementById("sex7").innerHTML=name7.Sex;
+ document.getElementById("position7").innerHTML=name7.Position;
+ document.getElementById("deo7").innerHTML=name7.enroledate;
+ 
+ //EIGHT ROW
+ document.getElementById("firstname8").innerHTML=name8.firstname;
+ document.getElementById("lastname8").innerHTML=name8.Lastname;
+ document.getElementById("age8").innerHTML=name8.Age;
+ document.getElementById("sex8").innerHTML=name8.Sex;
+ document.getElementById("position8").innerHTML=name8.Position;
+ document.getElementById("deo8").innerHTML=name8.enroledate;
+ 
+ //NINTH ROW
+ document.getElementById("firstname9").innerHTML=name9.firstname;
+ document.getElementById("lastname9").innerHTML=name9.Lastname;
+ document.getElementById("age9").innerHTML=name9.Age;
+ document.getElementById("sex9").innerHTML=name9.Sex;
+ document.getElementById("position9").innerHTML=name9.Position;
+ document.getElementById("deo9").innerHTML=name9.enroledate;
+
+ //TENTH ROW
+ document.getElementById("firstname10").innerHTML=name10.firstname;
+ document.getElementById("lastname10").innerHTML=name10.Lastname;
+ document.getElementById("age10").innerHTML=name10.Age;
+ document.getElementById("sex10").innerHTML=name10.Sex;
+ document.getElementById("position10").innerHTML=name10.Position;
+ document.getElementById("deo10").innerHTML=name10.enroledate;
+ 
+ 
